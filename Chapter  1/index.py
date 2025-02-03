caa5cd0bb036f4376bf4d31fc65abdd1bdafe00e
Anekantjainsagar <anekantjainsagar@gmail.com>
2025-02-03 23:29:49 +0530

    Completed Chapter 1

--- Chapter  1/index.py
+++ Chapter  1/index.py
@@ -1,4 +1,11 @@
 print("Hello world")
 
+print('''Hello, My name is
+    Anekant Jain''')
+
+
 # Pip - Package manager for python used to install modules in python
 # Modules - File containing code written by somebody else program
+
+# Hash keyword is used for the single line comments or (ctrl + /)
+# """ Txt between these will be commented for multiline """
