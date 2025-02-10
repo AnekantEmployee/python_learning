e4e24ec6f74b2fed7cbbb497145d39b1c66573d2
Anekantjainsagar <anekantjainsagar@gmail.com>
2025-02-10 23:31:51 +0530

    Added chapter 7 and partially 8

---
+++ Chapter 7/01_loops.py
@@ -0,0 +1,10 @@
+for i in range(1, 5, ): # 5 not included and 3rd value is a step
+    print(i)
+else:
+    print("Done") # Last item
+        
+print("\nWhile Loop: ")
+i = 1
+while(i<5):
+    print(i)
+    i += 1
